5ce0dbcf344b2a9f38fa538cd9864bb849d78388
klimov94 <94833276+klimov94@users.noreply.github.com>
2021-11-27 14:53:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let page = document.querySelector('.page');
 let themeButton = document.querySelector('.theme-button');
 themeButton.onclick = function() {
-page.classList.toggle('light-theme');
+page.classList.toggle('dark-theme');
 };
